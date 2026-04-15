917d6548709d99ea0ea9f732ffcc591d8949e3d4
Malere1 <adamudivine9@gmail.com>
2026-04-15 17:18:21 +0100

    Add files via upload

---
+++ TaskOne.java
@@ -0,0 +1,13 @@
+
+public class PrintSum {
+
+public static void main(String[]args)  {
+int total = 0;
+for(int counter=1;counter<=10;counter++) {
+     total += counter;
+    }
+    System.out.print("sum  is " + total);
+}
+
+
+}
---
+++ Tasktwo.java
@@ -0,0 +1,12 @@
+public class PrintAverage {
+
+public static void main(String[]args)  {
+int total = 0;
+for(int counter=1;counter<=10;counter++) {
+     total /= counter;
+    }
+    System.out.print("average  is " + total);
+}
+
+
+}
